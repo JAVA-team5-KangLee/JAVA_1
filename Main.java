431f8caefe71ab3200750cc3cf07b3c9096af4f6
Lee Jieun <wldmslove00@gmail.com>
2020-09-14 14:03:49 +0900

    Main

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(String[] args){
+        Rectangle rec = new Rectangle(4, 5);
+        Rectangle rec2 = new Rectangle(10, 2);
+        
+        System.out.println(rec.equals(rec2));
+    }
+}
