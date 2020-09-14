1400db9bfbcbd2c259770cf2817ce572e3950992
dabin0513 <dabin0513@naver.com>
2020-09-14 13:49:48 +0900

    team pro

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * 여기에 Main 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class Main
+{
+    public static void main(String[] args){
+        Rectangle rec = new Rectangle(4, 5);
+        Rectangle rec2 = new Rectangle(10, 2);
+        
+        System.out.println(rec.equals(rec2));
+    }
+}
